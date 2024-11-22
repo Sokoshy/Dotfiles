59130528584e186f1a949c43aae646e66d6735ca
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2024-11-22 19:10:28 +0100

    modify printing

--- hosts/modules/printing.nix
+++ hosts/modules/printing.nix
@@ -1,3 +1,12 @@
+{ pkgs, ... }:
 {
-    services.printing.enable = true;
+    services.printing = {
+    enable = true;
+    drivers = with pkgs; [ gutenprint canon-cups-ufr2 cups-filters ];
+    };
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
 }
