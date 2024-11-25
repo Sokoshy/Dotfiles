039ae87bf3fe2f6c5dbe3c24d22ea85178352e21
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2024-11-25 15:24:44 +0100

    modify language java

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -8,18 +8,21 @@
       editor = {
         lsp.display-messages = true;
       };
-      programs.helix.languages = {
-        language-server = {
+    };
+
+    # language settings
+
+    languages = {
+      java = {
+        syntax-highlighting = true;
+        treesitter-textobjects = true;
+        auto-indent = true;
+
+        language-servers = {
           jdtls = {
             command = "/nix/store/11n6d2avcj9gm03k7wbsmk84pzp45ckh-jdt-language-server-1.40.0/bin/jdtls";
-            args = [
-              "-data" "/home/${config.home.username}/.cache/jdtls/workspace"
-              ];
+            args = [];
           };
-        };
-        java = {
-          auto-format = true;
-          jdtls.javaHome = "/nix/store/48290hnlb13xmwjw9y16a1s785993bv7-openjdk-23-ga/lib/openjdk";
         };
       };
     };
