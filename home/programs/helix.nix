b8eac7f4eeacdc78359dcdadfeca0a09c5915886
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2024-11-25 11:25:57 +0100

    add java config and refacto

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -1,26 +1,27 @@
-{ pkgs, ...}:
+{ config, ...}:
 {
   programs.helix = {
-      enable = true;
-      package = pkgs.helix;
-  };
+    enable = true;
 
-  # config
-
-  editor = {
-      lsp.display-messages = true;
-    };
-  programs.helix.languages = {
-    language-server = {
-      jdtls = {
-        command = "${pkgs.jdt-language-server}/bin/jdtls";
-        args = [
-          "-data" "/home/${config.home.username}/.cache/jdtls/workspace"
-        ];
+    # config
+    settings = {
+      editor = {
+        lsp.display-messages = true;
+      };
+      programs.helix.languages = {
+        language-server = {
+          jdtls = {
+            command = "/nix/store/11n6d2avcj9gm03k7wbsmk84pzp45ckh-jdt-language-server-1.40.0/bin/jdtls";
+            args = [
+              "-data" "/home/${config.home.username}/.cache/jdtls/workspace"
+              ];
+          };
+        };
+        java = {
+          auto-format = true;
+          jdtls.javaHome = "/nix/store/48290hnlb13xmwjw9y16a1s785993bv7-openjdk-23-ga/lib/openjdk";
+        };
       };
     };
-    java = {
-      auto-format = true
-    }
   };
 }
