4055e8bfc5c5708a4c0a5ad05595fde8fdbb84d5
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2025-04-09 21:35:07 +0200

    indentation fix

--- home/programs/ia.nix
+++ home/programs/ia.nix
@@ -2,11 +2,11 @@
 {
   services = {
     ollama = {
-        enable = true;
-        acceleration = "rocm";
-        environmentVariables = {
-          HSA_OVERRIDE_GFX_VERSION = "10.3.0";
-        };
+      enable = true;
+      acceleration = "rocm";
+      environmentVariables = {
+        HSA_OVERRIDE_GFX_VERSION = "10.3.0";
+      };
     };
   };
   home.packages = with pkgs; [
