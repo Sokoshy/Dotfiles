2fbd98e253fad3578cdb5651ff92f794bb5f43c7
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2025-04-09 22:01:39 +0200

    fix bug

--- hosts/modules/ia.nix
+++ hosts/modules/ia.nix
@@ -1,6 +1,5 @@
 { pkgs, ...}:
 {
-  services.open-webui.enable = true;
     services = {
       ollama = {
         enable = true;
@@ -11,5 +10,4 @@
       };
     open-webui.enable = true;
   };
-  # services.open-webui.enable = true;
 }
