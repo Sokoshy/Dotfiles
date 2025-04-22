b8973f29546f9907f54c3fcf497446555a9bee9b
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2025-04-22 10:06:21 +0200

    fix(vscode): update package configuration to use fhsWithPackages

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -2,6 +2,9 @@
 {
     programs.vscode = {
     enable = true;
-    package = pkgs.vscode.fhs;
+    package = pkgs.vscode.fhsWithPackages (ps: with ps; [
+    sqlite
+    # Ajoutez ici d'autres paquets dont VS Code ou vos extensions pourraient avoir besoin dans l'environnement FHS
+  ]);
   };
 }
