fe2d451a8e8d10ebfe0723c20b70bbde812bd7df
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-02-24 20:40:49 +0100

    Add SSH agent configuration for Bitwarden in shell.nix

--- home/programs/shell.nix
+++ home/programs/shell.nix
@@ -2,5 +2,9 @@
    programs.bash = {
     enable = true;
     enableCompletion = true;
+    initExtra = ''
+      #bitwarden ssh
+      export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock"
+    '';
   };
 }
