76f5c702af8d8295465fe2bd60718a958eae2eea
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-03-05 19:17:13 +0100

    Fix controld.nix module

--- hosts/modules/controld.nix
+++ hosts/modules/controld.nix
@@ -3,15 +3,19 @@
 let
   hostname = config.networking.hostName;
   controld_stamp = if hostname == "nixos-desktop"
-    then config.sops.secrets.controld_stamp.desktop
+    then config.sops.secrets."controld_stamp_desktop".path
     else if hostname == "nixos-laptop"
-      then config.sops.secrets.controld_stamp.laptop
+      then config.sops.secrets."controld_stamp_laptop".path
       else null;
 in
 {
   # Déclarer les secrets
-  sops.secrets.controld_stamp.desktop = { };
-  sops.secrets.controld_stamp.laptop = { };
+  sops.secrets."controld_stamp_desktop" = {
+      key = "controld_stamp.desktop";
+    };
+    sops.secrets."controld_stamp_laptop" = {
+        key = "controld_stamp.laptop";
+      };
 
   # Configurer dnscrypt-proxy2 dynamiquement selon le hostname
   services.dnscrypt-proxy2 = lib.mkIf (controld_stamp != null) {
@@ -20,7 +24,7 @@ in
       server_names = [ "controld" ];
       static = {
         controld = {
-          stamp = builtins.readFile controld_stamp;
+          stamp = controld_stamp;
         };
       };
       listen_addresses = [ "127.0.0.1:53" ];
